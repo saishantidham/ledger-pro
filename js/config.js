cd51416b2c51d892efc9f73d5a8ad9efc83bd199
saishantidham <saishantidhamsociety.silvassa@gmail.com>
2026-06-27 18:52:37 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // Replace these with your actual Supabase project credentials
-const SUPABASE_URL = 'https://mxvaohzxqitktwzqwuix.supabase.co/rest/v1/';
+const SUPABASE_URL = 'https://mxvaohzxqitktwzqwuix.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im14dmFvaHp4cWl0a3R3enF3dWl4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI1NTQ5NzksImV4cCI6MjA5ODEzMDk3OX0.rHrKNXNuLJjkbQsLHitVtLylk7Aey3A3_mPFuqLhZus';
 
 // The CDN loads a global 'supabase' object.
